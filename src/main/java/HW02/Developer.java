768f5fd1451e2f4ecc060a39c69d234fc5397672
Misha <79090171508@yandex.ru>
2023-10-19 19:04:04 +0500

    HW02

---
+++ src/main/java/HW02/Developer.java
@@ -0,0 +1,6 @@
+package HW02;
+
+public abstract class Developer {
+    abstract void drinkCoffee();
+    abstract void smoke();
+}
---
+++ src/main/java/HW02/FrontEndDeveloper.java
@@ -0,0 +1,21 @@
+package HW02;
+
+public class FrontEndDeveloper extends Developer {
+
+    @Override
+    void drinkCoffee() {
+        System.out.println("drinkCoffee");
+    }
+
+    @Override
+    void smoke() {
+        System.out.println("smoke");
+    }
+
+
+    public void developGUI() {
+        System.out.println("create GUI");
+    }
+    
+
+}
