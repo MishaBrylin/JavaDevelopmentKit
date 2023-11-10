565a23159c3655dd7043fc9c7f6a3ba42014163c
Misha <79090171508@yandex.ru>
2023-11-10 18:48:45 +0500

    HW05

--- src/main/java/HW05/Main.java
+++ src/main/java/HW05/Main.java
@@ -1,12 +1,19 @@
 package HW05;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        ThePhilosopher pythagoras = new ThePhilosopher("Пифагор", 400);
-        ThePhilosopher heraclitus = new ThePhilosopher("Гераклид", 2000);
-        ThePhilosopher diogenes = new ThePhilosopher("Диоген", 6000);
-        ThePhilosopher socrates = new ThePhilosopher("Сократ", 700);
-        ThePhilosopher plato = new ThePhilosopher("Платон", 8000);
+        AtomicBoolean table = new AtomicBoolean(true);
+        ThePhilosopher pythagoras = new ThePhilosopher("Пифагор", 400, table);
+        ThePhilosopher heraclitus = new ThePhilosopher("Гераклид", 2000, table);
+        ThePhilosopher diogenes = new ThePhilosopher("Диоген", 6000, table);
+        ThePhilosopher socrates = new ThePhilosopher("Сократ", 700, table);
+        ThePhilosopher plato = new ThePhilosopher("Платон", 8000, table);
+
 
         pythagoras.start();
         heraclitus.start();
--- src/main/java/HW05/ThePhilosopher.java
+++ src/main/java/HW05/ThePhilosopher.java
@@ -1,24 +1,34 @@
 package HW05;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 public class ThePhilosopher extends Thread {
 
     private String name;
     private int thinks;
     private int eating = 0;
 
+    private static AtomicBoolean table;
 
-    public ThePhilosopher(String name, int thinks) {
+
+    public ThePhilosopher(String name, int thinks, AtomicBoolean table) {
         this.name = name;
         this.thinks = thinks;
+        this.table = table;
+
 
     }
     @Override
     public void run() {
         while (true) {
 
+            if (table.get() == true){
+
             System.out.println(name + " ест");
+            table = new AtomicBoolean(false);
             try {
                 Thread.sleep(500);
+                table = new AtomicBoolean(true);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
@@ -26,6 +36,7 @@ public class ThePhilosopher extends Thread {
             if (eating == 3) {
                 System.out.println(name + " наелся");
                 break;
+            }
             }
             System.out.println(name + " размышляет");
             try {
